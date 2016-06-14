9f730eee7db8af7fb6652697a255859a1d58811c
Sam Perkins <davidsp@princeton.edu>
2016-06-14 12:02:59 +0200

    day 12 morning

--- Day12/todo_script.js
+++ Day12/todo_script.js
@@ -10,9 +10,7 @@ app.controller("MainCtrl", function($scope) {
   }
 
   $scope.removeItem = function(item) {
-    console.log($scope.items);
     $scope.items.splice( $scope.items.indexOf(item), 1 );
-    console.log($scope.items);
   }
 
 });
